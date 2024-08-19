c633c48182515fc35f806e34c5eeb5500ed3075f
Ray Haddad <rayghaddad@gmail.com>
2024-08-19 18:55:57 +0300

    single car page proper header and footer

--- Readme.md
+++ Readme.md
@@ -1,1 +1,38 @@
 # Deals on wheels
+
+### Remove the default header (must redo after any wp update)
+
+In header.php remove this
+
+```
+
+<div id="header" role="banner">
+	<div id="headerimg">
+		<h1><a href="<?php echo home_url(); ?>/"><?php bloginfo( 'name' ); ?></a></h1>
+		<div class="description"><?php bloginfo( 'description' ); ?></div>
+	</div>
+</div>
+<hr />
+```
+
+### Remove the proudly by wordpress footer (must redo after any wp update)
+
+In footer.php remove this
+
+```
+<hr />
+<div id="footer" role="contentinfo">
+<!-- If you'd like to support WordPress, having the "powered by" link somewhere on your blog is the best way; it's our only promotion or advertising. -->
+	<p>
+		<?php
+		printf(
+			/* translators: 1: Site name, 2: WordPress */
+			__( '%1$s is proudly powered by %2$s' ),
+			get_bloginfo( 'name' ),
+			'<a href="https://wordpress.org/">WordPress</a>'
+		);
+		?>
+	</p>
+</div>
+</div>
+```
--- wp-content/plugins/motors-car-dealership-classified-listings/templates/single.php
+++ wp-content/plugins/motors-car-dealership-classified-listings/templates/single.php
@@ -1,72 +1,75 @@
 <?php get_header(); ?>
 <?php
-if ( class_exists( 'Elementor\Plugin' ) && class_exists( '\MotorsVehiclesListing\Elementor\Nuxy\TemplateManager' ) ) :
+if (class_exists('Elementor\Plugin') && class_exists('\MotorsVehiclesListing\Elementor\Nuxy\TemplateManager')) :
 	\MotorsVehiclesListing\Elementor\Nuxy\TemplateManager::motors_display_template();
 else :
 	get_header();
-	?>
-<div class="stm_single_car_wrapper">
-	<div class="stm_single_car_row">
-		<div class="stm_single_car_side">
-			<div class="stm-single-car-side">
+?>
+	<?php echo do_blocks('<!-- wp:template-part {"slug":"header","tagName":"header"} /-->'); ?>
 
-				<!--User info-->
-				<?php do_action( 'stm_listings_load_template', 'single-car/car-user', array( 'post_id' => get_the_ID() ) ); ?>
+	<div class="stm_single_car_wrapper">
+		<div class="stm_single_car_row">
+			<div class="stm_single_car_side">
+				<div class="stm-single-car-side">
 
-				<!--Prices-->
-				<?php do_action( 'stm_listings_load_template', 'single-car/car-price' ); ?>
+					<!--User info-->
+					<?php do_action('stm_listings_load_template', 'single-car/car-user', array('post_id' => get_the_ID())); ?>
 
-				<!--Buttons-->
-				<?php do_action( 'stm_listings_load_template', 'single-car/car-buttons' ); ?>
+					<!--Prices-->
+					<?php do_action('stm_listings_load_template', 'single-car/car-price'); ?>
 
-				<!--Data-->
-				<?php do_action( 'stm_listings_load_template', 'single-car/car-data' ); ?>
+					<!--Buttons-->
+					<?php do_action('stm_listings_load_template', 'single-car/car-buttons'); ?>
+
+					<!--Data-->
+					<?php do_action('stm_listings_load_template', 'single-car/car-data'); ?>
 
 
-				<!--CarGuru-->
-				<?php do_action( 'stm_listings_load_template', 'single-car/car-gurus' ); ?>
+					<!--CarGuru-->
+					<?php do_action('stm_listings_load_template', 'single-car/car-gurus'); ?>
 
-				<!--MPG-->
-				<?php do_action( 'stm_listings_load_template', 'single-car/car-mpg' ); ?>
+					<!--MPG-->
+					<?php do_action('stm_listings_load_template', 'single-car/car-mpg'); ?>
 
-				<!--Similar cars-->
-				<?php do_action( 'stm_listings_load_template', 'single-car/car-similar' ); ?>
+					<!--Similar cars-->
+					<?php do_action('stm_listings_load_template', 'single-car/car-similar'); ?>
+				</div>
+			</div>
+			<div class="stm_single_car_content">
+				<h2 class="title">
+					<?php
+					$as_label = apply_filters('motors_vl_get_nuxy_mod', false, 'show_generated_title_as_label');
+					echo wp_kses_post(apply_filters('stm_generate_title_from_slugs', get_the_title(get_the_ID()), get_the_ID(), $as_label));
+					?>
+				</h2>
+
+				<!--Actions-->
+				<?php do_action('stm_listings_load_template', 'single-car/car-actions'); ?>
+
+				<!--Gallery-->
+				<?php do_action('stm_listings_load_template', 'single-car/car-gallery'); ?>
+
+				<?php
+				if (have_posts()) :
+					while (have_posts()) :
+						the_post();
+						the_content();
+					endwhile;
+				endif;
+
+				/*Seller notes*/
+				do_action('stm_listings_load_template', 'single-car/car-seller-notes');
+
+				/* Features */
+				do_action('stm_listings_load_template', 'single-car/car-features');
+				?>
+
+				<!--Calculator-->
+				<?php do_action('stm_listings_load_template', 'single-car/car-calculator'); ?>
 			</div>
 		</div>
-		<div class="stm_single_car_content">
-			<h2 class="title">
-				<?php
-				$as_label = apply_filters( 'motors_vl_get_nuxy_mod', false, 'show_generated_title_as_label' );
-				echo wp_kses_post( apply_filters( 'stm_generate_title_from_slugs', get_the_title( get_the_ID() ), get_the_ID(), $as_label ) );
-				?>
-			</h2>
-
-			<!--Actions-->
-			<?php do_action( 'stm_listings_load_template', 'single-car/car-actions' ); ?>
-
-			<!--Gallery-->
-			<?php do_action( 'stm_listings_load_template', 'single-car/car-gallery' ); ?>
-
-			<?php
-			if ( have_posts() ) :
-				while ( have_posts() ) :
-					the_post();
-					the_content();
-				endwhile;
-			endif;
-
-			/*Seller notes*/
-			do_action( 'stm_listings_load_template', 'single-car/car-seller-notes' );
-
-			/* Features */
-			do_action( 'stm_listings_load_template', 'single-car/car-features' );
-			?>
-
-			<!--Calculator-->
-			<?php do_action( 'stm_listings_load_template', 'single-car/car-calculator' ); ?>
-		</div>
+		<?php do_action('stm_listings_load_template', '/single-car/search-results/search-results-carousel'); ?>
 	</div>
-	<?php do_action( 'stm_listings_load_template', '/single-car/search-results/search-results-carousel' ); ?>
-</div>
 <?php endif; ?>
 <?php get_footer(); ?>
+<?php echo do_blocks('<!-- wp:template-part {"slug":"footer","tagName":"footer"} /-->'); ?>

de21996f3bdc3532d7ea230dd559e9fb43290f51
Ray Haddad <rayghaddad@gmail.com>
2024-08-21 19:59:03 +0300

    refactoring and fixing empty p tags

--- wp-content/plugins/custom-featured-cars-shortcode/custom-car-shortcode.php
+++ wp-content/plugins/custom-featured-cars-shortcode/custom-car-shortcode.php
@@ -58,13 +58,16 @@ function fetchCars(int $limit)
         return [];
     }
 }
+add_filter('the_content', 'remove_empty_p', 11);
+function remove_empty_p($content)
+{
+    //return preg_replace('#<p>\s*+(<br\s*/*>)?\s*</p>#i', '', $content);
+    return preg_replace('#<p></p>#i', '', $content);
+}
 
-//  custom function to show dynamic featured contents
+// Custom function to show dynamic featured contents
 function custom_featured_cars_shortcode($atts)
 {
-    global $wpdb;
-
-
     // Attributes with default values
     $atts = shortcode_atts(
         array(
@@ -74,184 +77,94 @@ function custom_featured_cars_shortcode($atts)
         'custom_featured_cars'
     );
 
-    // Query to fetch cars from the database
-    // Adjust this query based on how your cars are stored in the database
-    $atts['limit'] = 6;
+    // Fetch cars from the database
+    // Assuming `fetchCars` is a function that retrieves cars based on the limit
     $cars = fetchCars($atts['limit']);
-    echo '<pre> end';
-    print_r($cars);
-    echo '</pre>';
-    // Start building the output
-    ob_start();
-    echo '
-    <!-- wp:group {"style":{"spacing":{"padding":{"top":"50px","bottom":"50px"}}},"className":"featured-car-section wow slideInRight delay-1000","layout":{"type":"constrained"}} -->
-    <div class="wp-block-group featured-car-section wow slideInRight delay-1000" style="padding-top:50px;padding-bottom:50px">
-    <!-- wp:heading {"textAlign":"center","style":{"typography":{"fontSize":"28px"},"color":{"text":"#222222"}}} -->
-    <h2 class="has-text-align-center has-text-color" style="color:#222222;font-size:28px">FEATURED CARS</h2>
-    <!-- /wp:heading -->
 
-    <!-- wp:image {"id":19,"sizeSlug":"full","linkDestination":"none","className":"title-img"} -->
-    <figure class="wp-block-image size-full title-img"><img src="' . esc_url(get_template_directory_uri()) . '/images/car-border.png" alt="" class="wp-image-19" /></figure>
-    <!-- /wp:image -->
+    if (!$cars) {
 
-    <!-- wp:paragraph {"align":"center","style":{"color":{"text":"#8c8282"},"typography":{"fontSize":"14px"}},"className":"head-text"} -->
-    <p class="has-text-align-center head-text has-text-color" style="color:#8c8282;font-size:14px">' . esc_html__("Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "auto-car-dealership") . '</p>
-    <!-- /wp:paragraph -->';
-
-    if ($cars) {
-        echo '<div class="wp-block-columns car-col">';
-        echo '<!-- wp:columns {"className":"car-col"} -->
-            <div class="wp-block-columns car-col">';
-        foreach (array_slice($cars, 0, 3) as $car) {
-?>
-            <!-- wp:column {"className":"car-box"} -->
-            <!-- <a href="<?php get_template_directory_uri() ?>/listings/mercedes-sls-amg/"> -->
-            <div class="wp-block-column car-box"><!-- wp:image {"id":41,"sizeSlug":"full","linkDestination":"none"} -->
-                <figure class="wp-block-image size-full"
-                    style="overflow: hidden; width:100%; aspect-ratio:3/2;"><img src="<?php echo $car['image'] ?>" alt="" class="wp-image-41" style="    object-fit: cover;
-    object-position: center;
-    width: 100%;
-    height: 100%;" /></figure>
-                <!-- /wp:image -->
-
-                <!-- wp:columns {"style":{"color":{"background":"#f0c541"}},"className":"price-col"} -->
-                <div class="wp-block-columns price-col has-background" style="background-color:#f0c541"><!-- wp:column -->
-                    <div class="wp-block-column">
-                        <!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"600"},"color":{"text":"#23393d"}},"className":"m-0 px-3"} -->
-                        <p class="has-text-align-center m-0 px-3 has-text-color" style="color:#23393d;font-size:16px;font-style:normal;font-weight:600"><?php echo esc_html__('NEW', 'auto-car-dealership'); ?></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-
-                    <!-- wp:column -->
-                    <div class="wp-block-column">
-                        <!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"600"},"color":{"text":"#23393d"}},"className":"location-text m-0 px-3"} -->
-                        <p class="has-text-align-center location-text m-0 px-3 has-text-color" style="color:#23393d;font-size:16px;font-style:normal;font-weight:600"><?php echo esc_html__($car['location'], 'auto-car-dealership'); ?></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-                </div>
-                <!-- /wp:columns -->
-
-                <!-- wp:heading {"level":4,"style":{"color":{"text":"#222222"},"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"700"}}} -->
-                <h4 class="has-text-color" style="color:#222222;font-size:14px;font-style:normal;font-weight:700"><?php echo esc_html__($car['title'], 'auto-car-dealership'); ?></h4>
-                <!-- /wp:heading -->
-
-                <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"14px"},"color":{"text":"#847e7e"}}} -->
-                <p class="has-text-color" style="color:#847e7e;font-size:14px;font-style:normal;font-weight:500"><?php echo esc_html__('Sed ut perspiciatis unde omnis iste natus error sit', 'auto-car-dealership'); ?></p>
-                <!-- /wp:paragraph -->
-
-                <!-- wp:columns {"className":"car-features"} -->
-                <div class="wp-block-columns car-features">
-                    <!-- wp:column {"style":{"color":{"background":"#f1eded","text":"#5d5252"}}} -->
-                    <div class="wp-block-column has-text-color has-background" style="color:#5d5252;background-color:#f1eded">
-                        <!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"12px","fontStyle":"normal","fontWeight":"600"},"color":{"text":"#5d5252"}},"className":"car-year"} -->
-                        <p class="has-text-align-center car-year has-text-color" style="color:#5d5252;font-size:12px;font-style:normal;font-weight:600"><?php echo esc_html__('2017', 'auto-car-dealership'); ?></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-
-                    <!-- wp:column {"style":{"color":{"background":"#f1eded"}}} -->
-                    <div class="wp-block-column has-background" style="background-color:#f1eded"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"12px","fontStyle":"normal","fontWeight":"600"},"color":{"text":"#5d5252"}},"className":"car-auto"} -->
-                        <p class="has-text-align-center car-auto has-text-color" style="color:#5d5252;font-size:12px;font-style:normal;font-weight:600"><?php echo esc_html__('Automatic', 'auto-car-dealership'); ?></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-
-                    <!-- wp:column {"style":{"color":{"background":"#f1eded"}}} -->
-                    <div class="wp-block-column has-background" style="background-color:#f1eded"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"12px","fontStyle":"normal","fontWeight":"600"},"color":{"text":"#5d5252"}},"className":"car-capacity"} -->
-                        <p class="has-text-align-center car-capacity has-text-color" style="color:#5d5252;font-size:12px;font-style:normal;font-weight:600"><?php echo esc_html__('21,000 ml', 'auto-car-dealership'); ?></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-                </div>
-                <!-- /wp:columns -->
-            </div>
-            <!-- </a> -->
-            <!-- /wp:column -->
-
-        <?php
-        }
-        echo ' </div>
-            <!-- /wp:columns --></div>';
-
-
-        // Second row of cars
-        echo '<div class="wp-block-columns car-col">';
-        echo '<!-- wp:columns {"className":"car-col"} -->
-            <div class="wp-block-columns car-col">';
-        foreach (array_slice($cars, 3, 6) as $car) {
-        ?>
-            <!-- wp:column {"className":"car-box"} -->
-            <div class="wp-block-column car-box"><!-- wp:image {"id":41,"sizeSlug":"full","linkDestination":"none"} -->
-                <figure class="wp-block-image size-full"><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/feature-car1.png" alt="" class="wp-image-41" /></figure>
-                <!-- /wp:image -->
-
-                <!-- wp:columns {"style":{"color":{"background":"#f0c541"}},"className":"price-col"} -->
-                <div class="wp-block-columns price-col has-background" style="background-color:#f0c541"><!-- wp:column -->
-                    <div class="wp-block-column"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"600"},"color":{"text":"#23393d"}},"className":"m-0 px-3"} -->
-                        <p class="has-text-align-center m-0 px-3 has-text-color" style="color:#23393d;font-size:16px;font-style:normal;font-weight:600"><?php echo esc_html__('$169,921', 'auto-car-dealership'); ?></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-
-                    <!-- wp:column -->
-                    <div class="wp-block-column"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"16px","fontStyle":"normal","fontWeight":"600"},"color":{"text":"#23393d"}},"className":"location-text m-0 px-3"} -->
-                        <p class="has-text-align-center location-text m-0 px-3 has-text-color" style="color:#23393d;font-size:16px;font-style:normal;font-weight:600"><?php echo esc_html__('Mumbai', 'auto-car-dealership'); ?></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-                </div>
-                <!-- /wp:columns -->
-
-                <!-- wp:heading {"level":4,"style":{"color":{"text":"#222222"},"typography":{"fontSize":"14px","fontStyle":"normal","fontWeight":"700"}}} -->
-                <h4 class="has-text-color" style="color:#222222;font-size:14px;font-style:normal;font-weight:700"><?php echo esc_html__('SILVER AUDI', 'auto-car-dealership'); ?></h4>
-                <!-- /wp:heading -->
-
-                <!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"500","fontSize":"14px"},"color":{"text":"#847e7e"}}} -->
-                <p class="has-text-color" style="color:#847e7e;font-size:14px;font-style:normal;font-weight:500"><?php echo esc_html__('Sed ut perspiciatis unde omnis iste natus error sit', 'auto-car-dealership'); ?></p>
-                <!-- /wp:paragraph -->
-
-                <!-- wp:columns {"className":"car-features"} -->
-                <div class="wp-block-columns car-features"><!-- wp:column {"style":{"color":{"background":"#f1eded","text":"#5d5252"}}} -->
-                    <div class="wp-block-column has-text-color has-background" style="color:#5d5252;background-color:#f1eded"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"12px","fontStyle":"normal","fontWeight":"600"},"color":{"text":"#5d5252"}},"className":"car-year"} -->
-                        <p class="has-text-align-center car-year has-text-color" style="color:#5d5252;font-size:12px;font-style:normal;font-weight:600"><?php echo esc_html__('2017', 'auto-car-dealership'); ?></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-
-                    <!-- wp:column {"style":{"color":{"background":"#f1eded"}}} -->
-                    <div class="wp-block-column has-background" style="background-color:#f1eded"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"12px","fontStyle":"normal","fontWeight":"600"},"color":{"text":"#5d5252"}},"className":"car-auto"} -->
-                        <p class="has-text-align-center car-auto has-text-color" style="color:#5d5252;font-size:12px;font-style:normal;font-weight:600"><?php echo esc_html__('Automatic', 'auto-car-dealership'); ?></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-
-                    <!-- wp:column {"style":{"color":{"background":"#f1eded"}}} -->
-                    <div class="wp-block-column has-background" style="background-color:#f1eded"><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"12px","fontStyle":"normal","fontWeight":"600"},"color":{"text":"#5d5252"}},"className":"car-capacity"} -->
-                        <p class="has-text-align-center car-capacity has-text-color" style="color:#5d5252;font-size:12px;font-style:normal;font-weight:600"><?php echo esc_html__('21,000 ml', 'auto-car-dealership'); ?></p>
-                        <!-- /wp:paragraph -->
-                    </div>
-                    <!-- /wp:column -->
-                </div>
-                <!-- /wp:columns -->
-            </div>
-            <!-- /wp:column -->
-
-<?php
-        }
-
-        echo '<!-- /wp:group --></div>';
-    } else {
-        echo '<p>No featured cars found.</p>';
+        return '<p>No featured cars found.</p>';
     }
+    // Temporarily disable wpautop
+    // Check if wpautop filter is active
+    if (has_filter('the_content', 'wpautop')) {
+        echo '<!-- wpautop is active -->';
+    } else {
+    }
+    echo '<!-- wpautop is NOT active -->';
+    remove_filter('the_content', 'wpautop');
+    // Start output buffering
+    ob_start();
+?>
+    <div class="wp-block-group featured-car-section wow slideInRight delay-1000" style="padding:50px 0;">
+        <h2 class="has-text-align-center has-text-color" style="color:#222222;font-size:28px">FEATURED CARS</h2>
+        <figure class="wp-block-image size-full title-img">
+            <img src="<?php echo esc_url(get_template_directory_uri()) . '/images/car-border.png'; ?>" alt="" class="wp-image-19" />
+        </figure>
+        <p class="has-text-align-center head-text has-text-color" style="color:#8c8282;font-size:14px">
+            <?php echo esc_html__("Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "auto-car-dealership"); ?>
+        </p>
 
-    // Return the content
-    return ob_get_clean();
+        <div class="wp-block-columns car-col" style="gap:2rem;">
+            <?php foreach ($cars as $car) : ?>
+                <div class="wp-block-column car-box">
+                    <figure class="wp-block-image size-full" style="overflow: hidden; width:100%; aspect-ratio:3/2;">
+                        <img src="<?php echo esc_url($car['image']); ?>" alt="" class="wp-image-41" style="object-fit: cover; object-position: center; width: 100%; height: 100%;" />
+                    </figure>
+
+                    <div class="wp-block-columns price-col has-background" style="background-color:#f0c541">
+                        <div class="wp-block-column">
+                            <p class="has-text-align-center m-0 px-3 has-text-color" style="color:#23393d;font-size:16px;font-style:normal;font-weight:600">
+                                <?php echo esc_html__('NEW', 'auto-car-dealership'); ?>
+                            </p>
+                        </div>
+
+                        <div class="wp-block-column">
+                            <p class="has-text-align-center location-text m-0 px-3 has-text-color" style="color:#23393d;font-size:16px;font-style:normal;font-weight:600"><?php echo esc_html__($car['location'], 'auto-car-dealership'); ?></p>
+                        </div>
+                    </div>
+
+                    <h4 class="has-text-color" style="color:#222222;font-size:14px;font-style:normal;font-weight:700">
+                        <?php echo esc_html__($car['title'], 'auto-car-dealership'); ?>
+                    </h4>
+
+                    <p class="has-text-color" style="color:#847e7e;font-size:14px;font-style:normal;font-weight:500">
+                        <?php echo esc_html__('Sed ut perspiciatis unde omnis iste natus error sit', 'auto-car-dealership'); ?>
+                    </p>
+
+                    <div class="wp-block-columns car-features">
+                        <div class="wp-block-column has-text-color has-background" style="color:#5d5252;background-color:#f1eded">
+                            <p class="has-text-align-center car-year has-text-color" style="color:#5d5252;font-size:12px;font-style:normal;font-weight:600">
+                                <?php echo esc_html__('2017', 'auto-car-dealership'); ?>
+                            </p>
+                        </div>
+
+                        <div class="wp-block-column has-background" style="background-color:#f1eded">
+                            <p class="has-text-align-center car-auto has-text-color" style="color:#5d5252;font-size:12px;font-style:normal;font-weight:600">
+                                <?php echo esc_html__('Automatic', 'auto-car-dealership'); ?>
+                            </p>
+                        </div>
+
+                        <div class="wp-block-column has-background" style="background-color:#f1eded">
+                            <p class="has-text-align-center car-capacity has-text-color" style="color:#5d5252;font-size:12px;font-style:normal;font-weight:600">
+                                <?php echo esc_html__('21,000 miles', 'auto-car-dealership'); ?>
+                            </p>
+                        </div>
+                    </div>
+                </div><?php endforeach; ?>
+        </div>
+    </div>
+<?php
+
+
+    $output = ob_get_clean();
+    //remove tabs, newlines, or any consecutive spaces from the output so they are not replaced with empty paragraphs
+    return preg_replace('/\r|\n|\t| {2,}/', '', $output);
 }
+
+// Register the shortcode
 function register_custom_featured_cars_shortcode()
 {
     add_shortcode('custom_featured_cars', 'custom_featured_cars_shortcode');
 }
 add_action('init', 'register_custom_featured_cars_shortcode');
-?>
